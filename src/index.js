c0f9b905d2b3d8ad0bbe4d491f9962fedbae05b5
Nathan Robbert <admin@bigrocketgames.com>
2017-11-11 12:24:18 -0800

    Lesson 5: complete

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './components/App';
 import reducer from './reducers'
 
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk, logger)
+  )
 )
 
 ReactDOM.render(
